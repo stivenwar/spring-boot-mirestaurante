bea4be1c325c295d8def8f37252ba6378cdbbad0
Esteban <92550679+StivenWarSecond@users.noreply.github.com>
2023-01-18 17:29:16 +0100

    start project

---
+++ src/main/java/com/mirestaurante/mirestaurante/SpringBootMirestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.mirestaurante.mirestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMirestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMirestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mirestaurante/mirestaurante/SpringBootMirestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirestaurante.mirestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMirestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
